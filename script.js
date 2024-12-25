ae34f3c489f86fe724617d3e8990390a403069f4
enolaworld <enolaworld@gmail.com>
2024-12-25 19:15:31 +0100

    remise a 700

--- script.js
+++ script.js
@@ -107,18 +107,12 @@ function switchStylesheet() {
   const mobileStylesheet = document.querySelector("link[href='./mobile.css']"); // Style pour Mobile
 
   // Active/Désactive les styles en fonction de la largeur de l'écran
-  if (viewportWidth > 960) {
+  if (viewportWidth > 700) {
     pcStylesheet.disabled = false;
     mobileStylesheet.disabled = true;
-    tabletStylesheet.disabled = true;
-} else if (viewportWidth <= 960 && viewportWidth > 700) {
-    pcStylesheet.disabled = false;
-    mobileStylesheet.disabled = true;
-    tabletStylesheet.disabled = true;
 } else {
     pcStylesheet.disabled = true;
     mobileStylesheet.disabled = false;
-    tabletStylesheet.disabled = true;
 }
 }
 
